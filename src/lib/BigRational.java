f1d0601e824e3109a1565f1c3a465c0f9b0562a4
Johan de Jong <johan.de.jong@sogeti.com>
2024-05-13 09:13:59 +0200

    Constants for 0 and 1 in BigRational.

--- src/lib/BigRational.java
+++ src/lib/BigRational.java
@@ -6,6 +6,9 @@ import java.math.RoundingMode;
 import java.util.Objects;
 
 public class BigRational implements Comparable<BigRational> {
+    private static final BigRational ZERO = new BigRational(0);
+    private static final BigRational ONE = new BigRational(1);
+
     private final BigInteger numerator;
     private final BigInteger denominator;
 
